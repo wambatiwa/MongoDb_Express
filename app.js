6c6343ef9f28e2d1c55cdc043c9fb3c6ec460790
wamba <wamb0019@algonquinlive.com>
2024-10-30 09:24:10 -0400

    reloading history

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+const path = require('path')
+
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.Mongo_Url)
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/api/stuff',stuffRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname,'images')))
+
+
+module.exports = app;
